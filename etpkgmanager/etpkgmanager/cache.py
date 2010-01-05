f3ce48ee12918b008873d49538b0190d7f195eb9
unknown <Tim@.(none)>
2010-01-05 08:40:40 -0800

    comments

--- etpkgmanager/etpkgmanager/cache.py
+++ etpkgmanager/etpkgmanager/cache.py
@@ -3,6 +3,7 @@
 __version__ = "0.1"
 __all__ = ["Cache"]
 
+import component
 
 # In EPM, patches are stored in the cache.  The path to the cache is found in
 # the template.  The (possibly temporary) path to the files or components that
@@ -10,8 +11,25 @@ __all__ = ["Cache"]
 # primary objective is to find components somewhere, and copy them into the
 # cache by their hexdigest.  If starting with bare files, the cache module
 # must first convert them into components with the component module.
+
+# cache tasks
+#
+# 1. copy components from <somewhere> to cache
+# 2. query template (or get from user) (or use default) path to cache
+# 3. if duplicate (cached component already present), call component.py on
+#    both, verify identical, inform user [INFO]
+# 4. identify self to other cache servers
+# 5. find other cache servers or caches
+# 6. if component requested, locate and return or None
+# 7. add components to peer component catalog
+
 class Cache(object):
     """DOCSTRING"""
     def __init__(self):
         pass
 
+    def cache(self, source):
+        """DOCSTRING"""
+        # if source is not a component, convert it first
+        #
+        # how to figure out if it is a component?
