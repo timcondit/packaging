58e5a1882862db0572b288961245b7bcec405f5c
Tim Condit <timc@envisioninc.com>
2010-01-04 17:51:26 -0800

    some early notes on what a cache is and does

--- etpkgmanager/etpkgmanager/cache.py
+++ etpkgmanager/etpkgmanager/cache.py
@@ -4,7 +4,12 @@ __version__ = "0.1"
 __all__ = ["Cache"]
 
 
-# In EPM, patches are stored in the cache.  
+# In EPM, patches are stored in the cache.  The path to the cache is found in
+# the template.  The (possibly temporary) path to the files or components that
+# go into the template comes from the user.  When populating a cache, the
+# primary objective is to find components somewhere, and copy them into the
+# cache by their hexdigest.  If starting with bare files, the cache module
+# must first convert them into components with the component module.
 class Cache(object):
     """DOCSTRING"""
     def __init__(self):
